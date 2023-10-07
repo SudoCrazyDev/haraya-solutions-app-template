8d61365b193ad5b631f93f4eaf8c06abfc558ace
Philip Louis Calub <philiplouis@codevidia.com>
2023-10-07 08:58:50 +0800

    Added Docker Compose for Frontend using nextjs

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    webpack: (config => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+            ignored: ['**/node_modules']
+        }
+        return config
+    })
+}
 
 module.exports = nextConfig
